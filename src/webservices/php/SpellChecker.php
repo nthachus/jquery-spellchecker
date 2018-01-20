65b90411523a0467b3bec7ca536b8a5aa0008360
Thach Nguyen <nthachus@gmail.com>
2018-01-20 16:52:53 +0700

    Handle errors on default SpellChecker.php service

--- src/webservices/php/SpellChecker.php
+++ src/webservices/php/SpellChecker.php
@@ -8,9 +8,17 @@
  * @license    https://github.com/badsyntax/jquery-spellchecker/blob/master/LICENSE-MIT
  */
 
-ini_set('display_errors', 1);
-
 require_once '../../../vendor/autoload.php';
 
 header('Access-Control-Allow-Origin: *');
-new \SpellChecker\Request();
+try {
+	new \SpellChecker\Request();
+
+} catch (Exception $e) {
+	if (function_exists('http_response_code'))
+		http_response_code(500);
+	else
+		header('HTTP/1.0 500 Internal Server Error');
+
+	\SpellChecker\Request::send_response(array('error' => $e->getMessage()));
+}
